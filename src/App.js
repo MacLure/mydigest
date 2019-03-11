0b69ec964038a19ad0c2de945648f70ae97b5348
Malcolm <malcolm.maclure@gmail.com>
2019-03-11 11:58:43 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,42 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import Landing from "./components/landing";
+import Main from "./components/main";
+import {TransitionGroup, CSSTransition,} from 'react-transition-group'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div style={styles.app}>
+        <div>
+          <Route render={({location}) =>(
+            <TransitionGroup>
+              <CSSTransition
+                key={location.key}
+                timeout={500}
+                classNames='fade'
+              >
+                <Switch location={location}>
+                  <Route exact path = '/' component = {Landing} />
+                  <Route path = '/main' component = {Main} />
+                </Switch>
+              </CSSTransition>
+            </TransitionGroup>
+          )} />
+        
       </div>
+    </div>
     );
   }
 }
 
 export default App;
+
+const styles={}
+
+styles.app={
+  position: 'relative',
+  width: '100vw',
+}
---
+++ src/components/landing.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Landing extends Component {
+  state = {  }
+  render() { 
+    return ( <div>Landing</div> );
+  }
+}
+ 
+export default Landing;
