203bac1f1974081a9b780f2b359a32e255297a7d
Malcolm <malcolm.maclure@gmail.com>
2019-03-13 11:23:04 -0400

    news options

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
-import  {getUSANews} from './../service';
 import ArticleCard from './articleCard'
+
+import  {getCanadaNews} from './../service';
+import  {getNews} from './../service';
+
 
 class Main extends Component {
 	state = {
-		articles: [],
-		refreshing: true
+    articles: [],
+    selectedCountry: null,
+    selectedTopic: 'general',
+    refreshing: true,
+    search: ''
 	};
 
 	componentDidMount = () => {
@@ -13,24 +19,120 @@ class Main extends Component {
 	};
 
 	fetchNews = () => {
-		getUSANews()
+		getCanadaNews()
 			.then(articles => {
 				this.setState({ articles, refreshing: false });
 			})
 			.catch(() => this.setState({ refreshing: false }));
-	};
+  };
+  
+updateCountry = (e) => {
+  this.setState({selectedCountry: e.target.value})
+  getNews(e.target.value, this.state.selectedTopic, this.state.search) 
+  .then(articles => {
+    this.setState({ articles, refreshing: false });
+  })
+  .catch(() => this.setState({ refreshing: false }));
+}
+
+updateTopic= (e) => {
+  this.setState({selectedTopic: e.target.value})
+  getNews(this.state.selectedCountry, e.target.value, this.state.search) 
+  .then(articles => {
+    this.setState({ articles, refreshing: false });
+  })
+  .catch(() => this.setState({ refreshing: false }));
+}
+
+handleChange = (e) => {
+  this.setState ({search: e.target.value})
+}
+
+submitSearch = (e) => {
+  getNews(this.state.selectedCountry, this.state.selectedTopic, this.state.search) 
+  .then(articles => {
+    this.setState({ articles, refreshing: false });
+  })
+  .catch(() => this.setState({ refreshing: false }));
+}
+
+
   render() { 
     return ( 
       <div>
 
         <div>
-        <select >
-          <option value="" selected>Select a country</option>
-          <option value="F">US</option>
-        </select>
-        
-        </div>
+          <select onChange={e => {this.updateCountry(e)}}>
+            <option value="">Select a country</option>
+            <option value='ae'>UAE</option>
+            <option value='ar'>Argentina</option>
+            <option value='at'>Austria</option>
+            <option value='au'>Australia</option>
+            <option value='be'>Belgium</option>
+            <option value='bg'>Bulgaria</option>
+            <option value='br'>Brazil</option>
+            <option value='ca'>Canada</option>
+            <option value='ch'>Switzerland</option>
+            <option value='cn'>China</option>
+            <option value='co'>Colombia</option>
+            <option value='cu'>Cuba</option>
+            <option value='cz'>Czech Republic</option>
+            <option value='de'>Germane</option>
+            <option value='eg'>Egypt</option>
+            <option value='fr'>France</option>
+            <option value='gb'>UK</option>
+            <option value='gr'>Greece</option>
+            <option value='hk'>Homg Kong</option>
+            <option value='hu'>Hungary</option>
+            <option value='id'>Indonesia</option>
+            <option value='ie'>Israel</option>
+            <option value='il'>Ireland</option>
+            <option value='in'>India</option>
+            <option value='it'>Italy</option>
+            <option value='jp'>Japan</option>
+            <option value='kr'>South Korea</option>
+            <option value='lt'>Lithuania</option>
+            <option value='lv'>Latvia</option>
+            <option value='ma'>Morocco</option>
+            <option value='mx'>Mexico</option>
+            <option value='my'>Malaysia</option>
+            <option value='ng'>Nigeria</option>
+            <option value='nl'>Netherlands</option>
+            <option value='no'>Norway</option>
+            <option value='nz'>New Zealand</option>
+            <option value='ph'>Phillipines</option>
+            <option value='pl'>Poland</option>
+            <option value='pt'>Portugal</option>
+            <option value='ro'>Romania</option>
+            <option value='rs'>Serbia</option>
+            <option value='ru'>Russia</option>
+            <option value='sa'>Soudi Arabia</option>
+            <option value='se'>Sweden</option>
+            <option value='sg'>Singapore</option>
+            <option value='si'>Slovenia</option>
+            <option value='sk'>Slovakia</option>
+            <option value='th'>Thailand</option>
+            <option value='tr'>Turkey</option>
+            <option value='tw'>Taiwan</option>
+            <option value='ua'>Ukraine</option>
+            <option value='us'>US</option>
+            <option value='ve'>Venezuela</option>
+            <option value='za'>South Africa</option>
+          </select>
 
+          <select onChange={e => {this.updateTopic(e)}}>
+          <option value="">Select a topic</option>
+          <option value='general'>general</option>
+          <option value='business'>business</option>
+          <option value='entertainment'>entertainment</option>
+          <option value='health'>health</option>
+          <option value='science'>science</option>
+          <option value='technology'>technology</option>
+          <option value='sports'>sports</option>
+        </select>
+        <input onChange = {e => this.handleChange(e)}type="text"></input>
+        <div onClick = {e => this.submitSearch(e)}>SEARCH</div>
+        </div>
 
         <div style={styles.articleCards} >
           {this.state.articles.map(article => (
--- src/service.js
+++ src/service.js
@@ -3,8 +3,14 @@ const endPointRoot = "https://newsapi.org/v2"
 const key = '086a7a7579144fe6a410f78be82dd0ff'
 
 
-export async function getUSANews() {
-  let result = await fetch(`${endPointRoot}/top-headlines?country=us&apiKey=${key}`)
+export async function getCanadaNews() {
+  let result = await fetch(`${endPointRoot}/top-headlines?country=ca&apiKey=${key}`)
+  .then(response => response.json());
+	return result.articles;
+}
+
+export async function getNews(country, topic, query) {
+  let result = await fetch(`${endPointRoot}/top-headlines?country=${country}&category=${topic}&q=${query}&apiKey=${key}`)
   .then(response => response.json());
 	return result.articles;
 }
