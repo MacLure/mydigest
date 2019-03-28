d63bd25b5f07b903eb023e13132bf54350f7596f
Malcolm <malcolm.maclure@gmail.com>
2019-03-28 15:06:33 -0400

    added weather icons

--- src/components/weather.jsx
+++ src/components/weather.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {getWeather} from './../service'; 
+import cloudy from './../assets/weatherIcons/cloudy.svg'
 
 class Weather extends Component {
   constructor(props) {
@@ -29,12 +30,27 @@ class Weather extends Component {
       .catch(() => this.setState({ refreshing: false }));
   };
 
+  conditions = [
+  'clear sky',
+  'few clouds',
+  'scattered clouds',
+  'broken clouds',
+  'shower rain',
+  'rain',
+  'thunderstorm',
+  'snow',
+  'mist',
+  ]
+
   render() { 
-    const weather = this.state.weather.description
+    const weather = this.state.weather.main
     const temp = (this.state.tempPerc.temp-273.15).toFixed(2)
 
     return ( 
       <div>
+        <div style={styles.weatherIcon}>
+          <img style={styles.weatherSVG} src={cloudy} />
+        </div>
         <div>{weather}</div>
         <div>{temp}<sup>o</sup>C</div>
       </div>
@@ -43,3 +59,15 @@ class Weather extends Component {
 }
  
 export default Weather;
+
+const styles = {}
+
+styles.weatherIcon = {
+  width: '10px',
+  height: '10px',
+}
+
+styles.weatherSVG = {
+  width: '100%',
+  height: '100%',
+}
